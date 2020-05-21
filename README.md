cfcc27b355d89f66b74ed8b59ed32cdc1e14dcc4
Dimitra Tatsiou <dimitratatsiou@gmail.com>
2020-05-21 14:24:30 +0300

    My second commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # statistic_covid_19
+
+## Αυτό είναι το πρώτο μου git repository
---
+++ go.R
@@ -0,0 +1,7 @@
+# This is my first R script
+
+# Loading required packages
+library(readxl)
+
+# Loading input files
+
